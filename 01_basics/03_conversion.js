6cfcf5ffa27d361a2ebcb6b1c9f1aa8614c52c0b
kritikayadav45 <143318367+kritikayadav45@users.noreply.github.com>
2025-12-10 12:46:56 +0000

    Operations

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -12,3 +12,14 @@ console.log(typeof(idInNumber))   // number datatype
 // in case of NULL -> it will give 0 on conversion
 // in case of undefined -> it will give NaN on conversion
 // while converting into boolean 1-> true; 0-> false; empty -> false; string -> true
+
+//***********************OPERATIONS****************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+ console.log("1"+2);     // 12
+ console.log("1"+2+2);   // 122
+ console.log(1+2+"2");   // 32
+ 
