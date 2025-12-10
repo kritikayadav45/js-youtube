d9dd2b4e6b288d9b373406464e8b0fd1918b9123
kritikayadav45 <143318367+kritikayadav45@users.noreply.github.com>
2025-12-10 13:23:00 +0000

    memeory/ strings

---
+++ 01_basics/memory.js
@@ -0,0 +1,10 @@
+// Stack and Heap memory 
+// for primitive  -> stack -> copy
+// for non- primitive -> heap -> reference to the original value
+
+//______________________strings________________________
+const name = "Kritika"
+const age = 20;
+
+console.log(`My name is ${name} and my age is ${age}.`);
+
